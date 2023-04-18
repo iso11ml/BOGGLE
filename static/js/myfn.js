51129312a5ffbba3cf7874588e93b2848bfb6479
iso11ml <oungrad201@gmail.com>
2023-04-18 01:13:22 -0600

    add_version_2

--- static/js/myfn.js
+++ static/js/myfn.js
@@ -2,17 +2,16 @@ import * as ko from "https://cdn.skypack.dev/knockout@3.5.1";
 
 function MyFn(settings = {}) {
 	const self = this;
-	//members
+
 	self.elID = settings.elID ?? "";
 	self.elToBind =
 		settings.elToBind ??
 		(self.elID ? document.querySelector("#" + self.ID) : null);
-	//observables
+
 	self.clickCount = ko.observable(0);
-	//funcs
-	//listen
+
 	window.addEventListener("resize", () => {});
-	// init
+
 	self.init = (function () {
 		if (self.elToBind) ko.applyBindings(self, self.elToBind);
 		else ko.applyBindings(self);
