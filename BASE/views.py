cb57545ff68900a1f535356cee28305111904bde
iso11ml <oungrad201@gmail.com>
2023-05-01 20:56:51 -0600

    add_heap_fibonacci

--- BASE/views.py
+++ BASE/views.py
@@ -84,6 +84,97 @@ class Trie:
             for line in file:
                 word = line.strip()
                 self.Insert(word)
+
+
+# Heap Para Almacenar La Palabra De Mayor Puntaje           
+import math
+
+# Creación del árbol
+class FibonacciTree:
+    def __init__(self, value, word):
+        self.value = value
+        self.word = word
+        self.child = []
+        self.order = 0
+
+    # Se adiciona el nuevo árbol al conjunto de árboles
+    def add_at_end(self, t):
+        self.child.append(t)
+        self.order = self.order + 1
+
+# Creando el heap 
+class FibonacciHeap:
+    def __init__(self):
+        self.trees = []
+        self.greatest = None
+        self.count = 0
+
+    # Insert a node
+    def insert_node(self, value, word):
+        new_tree = FibonacciTree(value, word)
+        self.trees.append(new_tree)
+        if (self.greatest is None or value > self.greatest.value):
+            self.greatest = new_tree
+        self.count = self.count + 1
+
+    # Obtener el máximo
+    def get_max(self):
+        if self.greatest is None:
+            return None
+        return self.greatest.word, self.greatest.value
+
+    # Extraer el máximo
+    def extract_max(self):
+        largest = self.greatest
+        if largest is not None:
+            for child in largest.child:
+                self.trees.append(child)
+            self.trees.remove(largest)
+            if self.trees == []:
+                self.greatest = None
+            else:
+                self.greatest = self.trees[0]
+                self.consolidate()
+            self.count = self.count - 1
+            return largest.word, largest.value
+
+    # Consolidación de la raíz
+    def consolidate(self):
+        aux = (floor_log(self.count) + 1) * [None]
+
+        while self.trees != []:
+            x = self.trees[0]
+            order = x.order
+            self.trees.remove(x)
+            while aux[order] is not None:
+                y = aux[order]
+                if x.value < y.value:
+                    x, y = y, x
+                x.add_at_end(y)
+                aux[order] = None
+                order = order + 1
+            aux[order] = x
+
+        self.greatest = None
+        for k in aux:
+            if k is not None:
+                self.trees.append(k)
+                if (self.greatest is None or k.value > self.greatest.value):
+                    self.greatest = k
+
+    # Muestra las raices así como el orden
+    def display_roots(self):
+        if self.trees:
+            print("Raices:")
+            for tree in self.trees:
+                print(f"{tree.word}: {tree.value} ({tree.order})")
+        else:
+            print("El heap Fibonacci está vacío")
+
+def floor_log(x):
+    return math.frexp(x)[1] - 1
+
+fibonacciHeap = FibonacciHeap()
 trie = Trie()
 trie.Insertar_archivo()
 global palabras
@@ -104,6 +195,7 @@ def verificar_existencia(request, word):
     if state == True:
         score = wordPuntuation(word)
         context = tabla_palabras(word)
+        fibonacciHeap.insert_node(score, word)
         print(context)
         return JsonResponse({'flag': state, 'score': score, 'word': word, 'words': context})
     else:
